ba00ee94c8b5ee7df53d96b5bb6837107ed74021
leylakhalilli <leylaxelilli65@gmail.com>
2022-10-27 18:49:28 +0400

    Initial commit

---
+++ src/main/java/com/example/programminglanguage/ProgrammingLanguageApplication.java
@@ -0,0 +1,13 @@
+package com.example.programminglanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingLanguageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammingLanguageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/programminglanguage/ProgrammingLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programminglanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingLanguageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
